d0722af1ddd9ffb5c78e7f9ffed6209d8d78c7e0
lovelinegatsi <lovelinegatsi@gmail.com>
2026-09-25 10:39:51 +0100

    chapitre-03: exercice7  tentative 2

--- chapitre-03/exo7-le_glisser_qui_sort/c3-exo7_main.cpp
+++ chapitre-03/exo7-le_glisser_qui_sort/c3-exo7_main.cpp
@@ -10,19 +10,17 @@ int nkmain(const NkEntryState &state) {
     (void)state;
 
     NkWindowConfig cfg;
-    cfg.title = "Ma fenetre";
+    cfg.title = "Capture souris";
     cfg.width = 1280;
     cfg.height = 720;
 
-    //cfg.minWidth = 600;
-    //cfg.minHeight = 400;
     cfg.resizable = true;
     cfg.movable = true;
-    cfg.closable =  true;
+    cfg.closable = true;
     cfg.minimizable = true;
     cfg.maximizable = true;
     cfg.canFullscreen = true;
-    cfg.modal = true;
+
     NkWindow window(cfg);
 
     if (!window.IsValid()) {
@@ -38,68 +36,44 @@ int nkmain(const NkEntryState &state) {
         }
     );
 
-    /*auto windowSize = window.GetSize();
-    auto surface = window.GetSurfaceDesc();
-
-    logger.Info(
-        "Fenetre : {} x {} | Surface : {} x {} | Facteur d'echelle (DPI) : {}",
-        windowSize.x,
-        windowSize.y,
-        surface.width,
-        surface.height,
-        window.GetDpiScale()
-    );*/
-    bool EstModifie = false;
-    events.AddEventCallback<NkWindowResizeEvent>(
-        [&](NkWindowResizeEvent *) {
-
-            auto taille = window.GetSize();
-
-            NkString title = cfg.title;
-
-            if (!EstModifie)
-                title += "*";
-
-            title += " - ";
-            title += NkString::Fmtf("%u", taille.x);
-            title += " x ";
-            title += NkString::Fmtf("%u", taille.y);
-
-            window.SetTitle(title);
-        }
-    );
-
+    // Déplacement de la souris
     events.AddEventCallback<NkMouseMoveEvent>(
         [&](NkMouseMoveEvent *e) {
-            events.AddEventCallback<NkMouseButtonPressEvent>(
-                [&](NkMouseButtonPressEvent *e) {
-                    if (e->IsLeft()) {
-                        std::cout << "Clic gauche : capture active" << std::endl;
-                        window.CaptureMouse(true);
-                    }
-                }
-            );
             std::cout << "Souris : "
-                << e->GetX()
-                << " ; "
-                << e->GetY()
-                << std::endl;
-            events.AddEventCallback<NkMouseButtonReleaseEvent>(
-                [&](NkMouseButtonReleaseEvent *e) {
-                    if (e->IsLeft()) {
-                        std::cout << "Clic gauche : capture liberee" << std::endl;
-                        window.CaptureMouse(false);
-                    }
-                }
-            );
+                      << e->GetX()
+                      << " ; "
+                      << e->GetY()
+                      << std::endl;
         }
     );
 
+    // Appui sur le bouton gauche : activation de la capture
+    events.AddEventCallback<NkMouseButtonPressEvent>(
+        [&](NkMouseButtonPressEvent *e) {
+            if (e->IsLeft()) {
+                std::cout << "Clic gauche : capture active"
+                          << std::endl;
+
+                window.CaptureMouse(true);
+            }
+        }
+    );
+
+    // Relâchement du bouton gauche : libération de la capture
+    events.AddEventCallback<NkMouseButtonReleaseEvent>(
+        [&](NkMouseButtonReleaseEvent *e) {
+            if (e->IsLeft()) {
+                std::cout << "Clic gauche : capture liberee"
+                          << std::endl;
+
+                window.CaptureMouse(false);
+            }
+        }
+    );
 
     while (running && window.IsOpen()) {
         events.PollEvents();
         NkClock::Sleep((int64)10);
-
     }
 
     window.Close();
